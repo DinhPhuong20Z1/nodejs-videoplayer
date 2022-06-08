a72b98b13fefa2526188e5710d657811ef224733
DinhPhuong20Z1 <phuongtunkim2001@gmail.com>
2022-06-08 09:17:34 +0700

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const app = require('express')()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
+const fs = require("fs");
 
 var screen
 
@@ -24,11 +25,16 @@ io.on('connection', (socket) => {
     })
 
     socket.on('sendToScreen', (data) => {
+        console.log('screen',screen)
         io.to(screen.id).emit('fromRemote', data)// relay from remote to screen!
     })
 
     socket.on('changeColor', () => {
         io.to(screen.id).emit('remoteWantsNewColor', socket.id)
+    })
+
+    socket.on('changeVideo', (data) => {
+        io.to(screen.id).emit('remoteWantsVideo', data)
     })
 
     socket.on('disconnect', (data) => {
@@ -39,5 +45,41 @@ io.on('connection', (socket) => {
         io.to(data).emit('thanks', socket.id)
     })
 })
+
+app.get("/video", function (req, res) {
+    // Ensure there is a range given for the video
+    const range = req.headers.range;
+    if (!range) {
+      res.status(400).send("Requires Range header");
+    }
+  
+    // get video stats (about 61MB)
+    const videoPath = "bigbuck.mp4";
+    const videoSize = fs.statSync("bigbuck.mp4").size;
+  
+    // Parse Range
+    // Example: "bytes=32324-"
+    const CHUNK_SIZE = 10 ** 6; // 1MB
+    const start = Number(range.replace(/\D/g, ""));
+    const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+  
+    // Create headers
+    const contentLength = end - start + 1;
+    const headers = {
+      "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+      "Accept-Ranges": "bytes",
+      "Content-Length": contentLength,
+      "Content-Type": "video/mp4",
+    };
+  
+    // HTTP Status 206 for Partial Content
+    res.writeHead(206, headers);
+  
+    // create video read stream for this particular chunk
+    const videoStream = fs.createReadStream(videoPath, { start, end });
+  
+    // Stream the video chunk to the client
+    videoStream.pipe(res);
+  });
 
 server.listen(3000)
